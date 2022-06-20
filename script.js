699ce92ddfef6e59ef5fcab2c67a404fbef8f70f
Vitkarus <nikitinskiy.viktor@gmail.com>
2022-06-20 12:02:52 +0300

    Fixes

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ var introTextsArray = [
     // "nice to meet you.",
     "still here? that's cool.",
     "kept you waiting, huh?",
-    "stop right there, criminal scum!",
+    // "stop right there, criminal scum!",
     "hello there.",
     "bottom text. upper text.",
     "why can't safari handle my site :/",
@@ -246,7 +246,7 @@ function changeIntro()
 {
     var number = Math.floor(Math.random() * introTextsArray.length);
     var introText = introTextsArray[number];
-    if (number == 7){document.documentElement.style.setProperty('--intro-main-text-font', "ComicSans");}
+    if (number == 6){document.documentElement.style.setProperty('--intro-main-text-font', "ComicSans");}
     else {document.documentElement.style.setProperty('--intro-main-text-font', "Square721");};
     // var introWords = introText.split(/(\s+)/);
     var introWords = introText.split(" ");
